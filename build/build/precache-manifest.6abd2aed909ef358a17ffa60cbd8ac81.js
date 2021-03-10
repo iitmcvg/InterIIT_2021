c9cf97746690bfc0da3c58fc552b0afbd35fa610
Mukesh V <mukeshvinayak2001@gmail.com>
2021-03-10 23:14:31 +0530

    One step for a Man

---
+++ build/build/precache-manifest.6abd2aed909ef358a17ffa60cbd8ac81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc6a08463036f9591bc15c7b21431e11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5621aeb5b6afd6cad7df",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "7edd90d5bc5e3d25ab38",
+    "url": "/static/css/main.1fd27b09.chunk.css"
+  },
+  {
+    "revision": "5621aeb5b6afd6cad7df",
+    "url": "/static/js/2.b46dbd57.chunk.js"
+  },
+  {
+    "revision": "e933f8dbfdf496cb72e495663a499a00",
+    "url": "/static/js/2.b46dbd57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7edd90d5bc5e3d25ab38",
+    "url": "/static/js/main.9d558499.chunk.js"
+  },
+  {
+    "revision": "0255ff7d238c482ca13a",
+    "url": "/static/js/runtime-main.6ede62d1.js"
+  }
+]);
