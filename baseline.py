7162d9f58a24090f91d4767ee00e845266f1a418
shreyesss <64736777+shreyesss@users.noreply.github.com>
2021-03-20 02:38:54 +0530

    Add files via upload

--- baseline.py
+++ baseline.py
@@ -2,31 +2,34 @@ import cv2
 import glob
 import pandas as pd
 import  numpy as np
-from  tensorflow.keras.models import Sequential
-from  tensorflow.keras.layers import Conv2D,Dropout,Dense,Flatten,MaxPooling2D
+from  tensorflow.keras.models import Sequential,Model
+from  tensorflow.keras.layers import Input,Conv2D,Dropout,Dense,Flatten,MaxPooling2D,GlobalAveragePooling2D
+from tensorflow.keras.preprocessing import  image
 from tensorflow.keras.optimizers import SGD
 from tensorflow.keras.callbacks import ModelCheckpoint,LearningRateScheduler
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report,confusion_matrix
+from tensorflow.keras.applications.vgg19 import preprocess_input,VGG19
+from tensorflow.keras.backend import clear_session
+clear_session()
+
+
 NUM_CLASSES = 43
 IMG_SIZE = 48
 
 def read_train_data():
+    print('wegfgd')
     dir = r'C:\Users\Shreyas\PycharmProjects\inter-IIT\archive'
-    df1 = pd.read_csv(dir + '\\' + 'train.csv')
+    df1 = pd.read_csv(dir + '\\' + 'Train_augmented.csv')
     paths = df1['Path']
     class_id_train = df1['ClassId']
-
+    print('------reading images--------')
 
     images_train = []
     for path in paths:
-        img = preprocess_img(cv2.imread(dir + '\\' + path))
+        img = cv2.imread(dir + '\\' + path)
         images_train.append(img)
-
-
-
-
 
     return  np.array(images_train),np.eye(NUM_CLASSES)[class_id_train]
 
@@ -37,26 +40,28 @@ def read_test_data():
     paths = df2['Path']
     class_id_test = df2['ClassId']
 
+    print('------reading images--------')
     images_test = []
     for path in paths:
-        images_test.append(cv2.resize(cv2.imread(dir + '\\' + path), (IMG_SIZE, IMG_SIZE)))
+        images_test.append(cv2.resize(cv2.imread(dir + '\\' + path), (IMG_SIZE, IMG_SIZE))  )
 
     return  np.array(images_test), np.eye(NUM_CLASSES)[class_id_test]
 
-def preprocess_img(img):
-    # Histogram normalization in v channel
+def visualize(X,Y):
+    print('----visualize dataset-------')
 
-    # central square crop
-    min_side = min(img.shape[:-1])
-    centre = img.shape[0] // 2, img.shape[1] // 2
-    img = img[centre[0] - min_side // 2:centre[0] + min_side // 2,
-              centre[1] - min_side // 2:centre[1] + min_side // 2,
-              :]
-    img = img/255.0
+    idxs = np.random.randint(low=0,high=X.shape[0],size=20)
+    image = X[idxs]
+    label = Y[idxs]
 
-    # rescale to standard size
-    img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
-    return img
+
+    fig = plt.figure(figsize=(22, 22))
+    for i in range(20):
+        ax = fig.add_subplot(4, 5, i+1, xticks=[], yticks=[])
+        ax.imshow(image[i])
+        ax.set_title(f"Label: {label[i]}")
+    plt.show()
+
 
 
 
@@ -90,12 +95,39 @@ def cnn_model(train=True):
     model.add(Dense(512, activation='relu'))
     model.add(Dropout(0.5))
     model.add(Dense(NUM_CLASSES, activation='softmax'))
-    if train == False:
-        model.load_weights('model.h5')
+    if train == False  :
+        model.load_weights('model_aug.h5')
     model.summary()
     return model
 
 
+def cnn_model_VGG19(train=True):
+    base_model = VGG19(weights='imagenet', include_top=False)
+
+    # add a global spatial average pooling layer
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    # let's add a fully-connected layer
+    x = Dense(1024, activation='relu')(x)
+    # and a logistic layer -- let's say we have 200 classes
+    x = Dense(512, activation='relu')(x)
+    predictions = Dense(NUM_CLASSES, activation='softmax')(x)
+
+    # this is the model we will train
+    model = Model(inputs=base_model.input, outputs=predictions)
+
+    # first: train only the top layers (which were randomly initialized)
+    # i.e. freeze all convolutional InceptionV3 layers
+    for layer in base_model.layers:
+        layer.trainable = False
+
+    # compile the model (should be done *after* setting layers to non-trainable)
+   # model.compile(optimizer='rmsprop', loss='categorical_crossentropy')
+
+    if train == False  :
+        model.load_weights('model_VGG19.h5')
+    model.summary()
+    return model
 
 
 def train(X,Y):
@@ -113,14 +145,14 @@ def train(X,Y):
         return lr * (0.1 ** int(epoch / 10))
 
     batch_size = 32
-    epochs = 20
+    epochs = 40
 
     history = model.fit(X_train, Y_train,
                         batch_size=batch_size,
                         epochs=epochs,
                         validation_data=(X_val, Y_val),
                         callbacks=[LearningRateScheduler(lr_schedule),
-                                   ModelCheckpoint('model.h5', save_best_only=True)], verbose=1)
+                                   ModelCheckpoint('model_aug.h5', save_best_only=True)], verbose=1)
 
     plt.plot(history.history['acc'])
     plt.plot(history.history['val_acc'])
@@ -162,10 +194,22 @@ def evaluate(Xt,Yt):
     plt.imshow(conf_mat,cmap='Reds')
     plt.show()
 
+    list_failures = []
+    for i in range(len(y_pred)):
+        if y_true[i]!=y_pred[i]:
+            list_failures.append(Yt[i])
+            plt.imsave('failure'+str(i)+'.png',Xt[i])
+            #plt.imshow(Xt[i])
+            #plt.show()
+    print(len(list_failures))
 
 
 
-X,Y = read_train_data()
-train(X,Y)
+
+
+
+#X,Y = read_train_data()
+#visualize(X,Y)
+#train(X,Y)
 Xt,Yt = read_test_data()
 evaluate(Xt,Yt)
