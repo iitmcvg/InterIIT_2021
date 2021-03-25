f9f012bb5b71243acdd0eccdbec81d9417ea1b98
Irfaan Arif <irfaan365@gmail.com>
2021-03-25 22:22:45 +0530

    Add activation_maximization_map

--- model_viz.py
+++ model_viz.py
@@ -142,6 +142,35 @@ def activation_maps(model_path, test_dir, layers_name, viz_classes, run_id, img_
             with mlflow.start_run(run_id=run_id):
                 mlflow.log_artifact(path)
 
+def activation_maximization_map(model_path):
+    # Layer name to inspect
+    layer_name = 'block3a_activation'
+
+    epochs = 100
+    step_size = 1.
+    filter_index = 0
+
+    # Create a connection between the input and the target layer
+    model_full = tf.keras.models.load_model(model_path)
+    submodel = model_full.get_layer('efficientnetb0')
+
+    # Initiate random noise
+    input_img_data = np.random.random((1, 224, 224, 3))
+    input_img_data = (input_img_data - 0.5) * 20 + 128.
+
+    # Cast random noise from np.float64 to tf.float32 Variable
+    input_img_data = tf.Variable(tf.cast(input_img_data, tf.float32))
+
+    # Iterate gradient ascents
+    for _ in range(epochs):
+        with tf.GradientTape() as tape:
+            outputs = submodel(input_img_data)
+            loss_value = tf.reduce_mean(outputs[:, :, :, filter_index])
+        grads = tape.gradient(loss_value, input_img_data)
+        normalized_grads = grads / (tf.sqrt(tf.reduce_mean(tf.square(grads))) + 1e-5)
+        input_img_data.assign_add(normalized_grads * step_size)
+    cv2.imsave("run_latest/activation_max_map",input_img_data)
+
 def visualize_main(test_dir, model_path, run_id, viz_classes):
     test_dir = "/home/lordgrim/Final_interiit/datasets/Train_dummy/"
     model_path = "/home/lordgrim/Final_interiit/final_model_test.h5"
