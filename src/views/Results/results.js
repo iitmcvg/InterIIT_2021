cbb4a5f69f1861987de094d2d4ffca8e21b65acb
Adil-MohammedK <adilmohammed2000@outlook.com>
2021-03-25 12:39:01 +0530

    Get confsuion matrix from backend

--- src/views/Results/results.js
+++ src/views/Results/results.js
@@ -1,27 +1,55 @@
 import React from 'react';
-import { Grid, Button } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 
-class Results extends React.Component {
+import { socket } from '../../components/Socket';
+
+export default class ResultsGrid extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      scores: 0,
-      accuracy: 0,
       images: [],
+      c: 0,
+      data: [],
     };
   }
-  componentDidMount() {}
+  getData = (data) => {
+    const blob = new Blob([data['img']], { type: 'image/png' });
+    var newlist = [];
+    if (this.state.c === data['c']) newlist = this.state.images;
+    newlist.push(blob);
+    this.setState({
+      images: newlist,
+      c: data['c'],
+    });
+  };
+
+  componentDidMount() {
+    socket.on('results', this.getData);
+    socket.emit('results', {});
+  }
+  componentWillUnmount() {
+    socket.off('results');
+  }
   render() {
     return (
-      <div>
-        <Grid
-          item
-          xs={4}
-          style={{ height: '500px', overflowY: 'scroll' }}
-        ></Grid>
-      </div>
+      <Grid container spacing={0} style={{ padding: '10px' }}>
+        {this.state.images &&
+          this.state.images.map((item, index) => {
+            return (
+              <Grid xs={6} item key={index}>
+                <img
+                  src={URL.createObjectURL(item)}
+                  style={{
+                    position: 'inherit',
+                    height: '100%',
+                    width: '100%',
+                  }}
+                  alt={index}
+                />
+              </Grid>
+            );
+          })}
+      </Grid>
     );
   }
 }
-
-export default Results;
