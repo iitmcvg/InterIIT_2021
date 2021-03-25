5ce2f13cd747a1a14c506f9dd330cfbde5e3ef82
Mukesh V <mukeshvinayak2001@gmail.com>
2021-03-25 22:46:12 +0530

    removed yaml, added aug, viz, train

---
+++ augmentations.py
@@ -0,0 +1,110 @@
+import albumentations as A
+import cv2
+import glob
+import numpy as np
+import random
+import os
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import csv
+from itertools import cycle,islice
+import json
+
+IMG_SHAPE = 48
+NUM_CLASSES = 43
+
+dir_load = {'op' : r'./data/', 'preview' : r'./data-pre/'}
+dir_save = {'op' : r'./data-augmented/', 'preview' : r'./data-post/'}
+
+class augmentation:
+    def __init__(self,class_id,dict_transform,transform_vals,num_target_imgs, flag):
+        self.class_id = class_id
+        self.target = num_target_imgs
+        self.transform_vals = transform_vals
+        self.flag = flag
+
+    def load_data(self, train_dir, new_train_dir):
+        print('reading class id : {}'.format(self.class_id))
+
+        if self.flag == "op":
+            self.dir_load = train_dir
+            self.dir_save = new_train_dir
+        else:
+            self.dir_load = dir_load[self.flag]
+            self.dir_save = dir_save[self.flag]
+
+        self.dir = str(self.class_id)
+        if self.flag == "op":
+            self.path_load = os.path.join(self.dir_load, self.dir)
+        else:
+            self.path_load = self.dir_load
+        self.imgs = [cv2.imread(file) for file in
+                glob.glob(self.path_load + "/*.png")]
+        self.len = len(self.imgs)
+
+        print('number of images : {}'.format(self.len))
+
+    def setup_pipeline(self, dict_transform):
+
+        tranform_list = []
+        if 'shadow' in dict_transform: tranform_list.append(A.RandomShadow(shadow_roi=(0, 0.5, 1, 1),num_shadows_upper=1,p=0.2))
+        if 'scale' in dict_transform: tranform_list.append(A.RandomScale(scale_limit=float(dict_transform['scale'])))
+        if 'rotate' in dict_transform: tranform_list.append(A.Rotate(limit=float(dict_transform['rotate']),p=0.8))
+        if 'shift' in dict_transform: tranform_list.append( A.ShiftScaleRotate(shift_limit=float(dict_transform['shift']), scale_limit=0.0, rotate_limit= 0, interpolation= 1, border_mode=4, p=0.8))
+        if 'brightness' in dict_transform: tranform_list.append(A.RandomBrightness(limit=float(dict_transform['brightness']),p=0.8))
+        if 'contrast' in dict_transform: tranform_list.append(A.RandomContrast(limit=float(dict_transform['contrast']), p=0.8))
+        if 'motion_blur' in dict_transform: tranform_list.append(A.MotionBlur(p=0.5, blur_limit=7))
+        if 'fog' in dict_transform: tranform_list.append(A.RandomFog(fog_coef_lower=0.0, fog_coef_upper=float(dict_transform['fog']), alpha_coef=0.05, p=0.7))
+        if 'rain' in dict_transform:tranform_list.append(A.RandomRain(brightness_coefficient=0.95, drop_width= 1, blur_value=1, p=0.7))
+        if 'occlusion' in dict_transform:tranform_list.append(A.CoarseDropout(max_holes=5, max_height=8, max_width=8,p=0.5))
+        self.transform = A.Compose(tranform_list)
+
+    def augment(self,save):
+        transformed_imgs = []
+        data = []
+        count = 0
+
+        if save :
+            if self.flag == "preview":
+                self.path_save = self.dir_save
+            else:
+                self.path_save = os.path.join(self.dir_save, self.dir)
+
+            if not self.dir in os.listdir(self.dir_save) and self.flag == "op":
+                os.mkdir(self.path_save)
+
+        for img in list(islice(cycle(self.imgs),0,self.target)):
+            img = cv2.resize(img, (224, 224))
+            transformed_img = self.transform(image = img)['image']
+            transformed_imgs.append(transformed_img)
+            if save :                                                                  ### saving image to directory
+                cv2.imwrite(self.path_save + '/' + str(count) + '.png', transformed_img)
+                data.append([count, '\Train_augmented' + '\\' + self.dir + '/' + str(count) + '.png'])
+                count += 1
+
+        print('class saved ' + str(self.class_id))
+
+        if self.flag == "op":
+            with open(self.dir_save+'archive\Train_augmented.csv', 'a', newline='') as file:       ### saving image name and path to a CSV file
+                writer = csv.writer(file)
+                writer.writerows(data)
+
+        return transformed_imgs
+
+def generate_augented_dataset(config_json, train_dir, new_train_dir):
+    for i in config_json['dir']:
+
+        if i == 35 or i == 38 or i == 39:  #### not rotating classes 35,38,39
+            transform_list = config_json['ops']
+        else:
+            transform_list = config_json['ops']
+            transform_list['rotate'] = 0
+
+        transform_vals = {"shift_factor": 0.2, "scale_factor": 0.2, "rot_angle": 60, "brightness_factor": 0.1,
+                        "contrast_factor": 0.2, "motion_blur_factor": 4, "fog_factor": 0.4, "rain_factor": 3,  'generate_occlusion':1}
+
+        aug = augmentation(class_id=i, dict_transform=transform_list, transform_vals=transform_vals,
+                        num_target_imgs=23, flag = 'op')
+        aug.load_data( train_dir, new_train_dir)
+        aug.setup_pipeline(dict_transform=transform_list)
+        aug.augment(save=True)
--- server.py
+++ server.py
@@ -13,23 +13,13 @@ from flask           import Flask, request
 from flask_cors      import CORS
 from flask_socketio  import SocketIO, emit, disconnect
 
+from model_viz import visualize_main
 from augment import augmentation
 import model_eval as modelEval
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--prod",action='store_true',help="Production configs are applied")
-args = vars(parser.parse_args())
-
-if args['prod']:
-    config_file = 'prod.yaml'
-    warnings.filterwarnings('ignore')
-    print('Running in Production')
-else:
-    config_file = 'dev.yaml'
-    print('Running in Dev')
-
-with open(config_file) as f:
-    config = yaml.load(f)
+from model_eval import *
+from augmentations import *
+from start_train import train_classifier
 
 app = Flask(__name__, static_folder="./build", static_url_path="/")
 socketio = SocketIO(app, cors_allowed_origins='*', logger=True)
@@ -83,7 +73,19 @@ def handle_input(results):
     file.close()
     pca_analysis=pca.read()
     data = r.read()
-    emit('results',{"img": data,"matrix": matrixValues,"pca": pca_analysis})
+    train = open("./run_latest/finetune_train_acc.png",'rb')
+    finetune_acc = train.read()
+    train.close()
+    train = open('./run_latest/finetune_train_loss.png','rb')
+    finetune_loss = train.read()
+    train.close()
+    train = open('./run_latest/full_train_loss.png','rb')
+    full_loss = train.read()
+    train.close()
+    train = open('./run_latest/full_train_loss.png','rb')
+    full_acc = train.read()
+    train.close()
+    emit('results',{"img": data,"matrix": matrixValues,"pca": pca_analysis,'finetune_acc': finetune_acc,'finetune_loss': finetune_loss,'full_loss': full_loss,'full_acc': full_acc})
     r.close()
     pca.close()
 
@@ -154,10 +156,55 @@ def handle_input(data):
     dump.close()
 
 @socketio.on('train')
-def handle_input(json):
-    print(json)
+def handle_input(optionsDict):
+    path_to_aug_json = "aug.json"
+    path_to_defaug_json = "defaug.json"
+
+    with open(path_to_aug_json) as f:
+        aug = json.load(f)
+
+    with open(path_to_defaug_json) as f:
+        defaug = json.load(f)
+
+    train_data_root = "train/"
+    new_train_data_root = "train_augmented/"
+    test_data_root = "test/"
+    run_id = 'a56e04bce6fc41949f03f187221be156'
+
+    os.makedirs(new_train_data_root,exist_ok=True)
+
+    weight_mapping = {
+        'Pretrained weights': 'pretrained',
+        'Best Weights': 'best_weights',
+        'From scratch': 'scratch'
+        }
+
+    model_mapping = {
+        'EfficientNet': 'efficientnet',
+        '2 Layer ConvNet': '2layer_conv',
+        'ResNet50': 'resnet'
+        }
+
+    # Choosing augmentation
+    if optionsDict['aug'] == 'Configured params':
+        generate_augented_dataset(aug, train_data_root, new_train_data_root)
+    elif optionsDict['aug'] == 'Default params':
+        generate_augented_dataset(defaug, train_data_root, new_train_data_root)
+    elif optionsDict['aug'] == 'No augmentation':
+        new_train_data_root = train_data_root
+
+    # model_path,run_id = train_classifier(new_train_data_root, test_data_root, train_type=weight_mapping[optionsDict['weights']], cnn_model=model_mapping[optionsDict['model']])
+
+    # ## Model evaluation
+    # model_path = "final_model_test.h5"
+    # model_eval_fns(test_data_root, model_path, run_id)
+
+    # ##Model visulalization
+    # viz_classes = ['2','5']
+    # visualize_main(test_data_root, model_path, run_id, viz_classes)
+    return emit('train', True)
 
 if __name__ == '__main__':
-    app.config['SECRET_KEY'] = config['socket_secret']
+    app.config['SECRET_KEY'] = 'BlaBlaBla'
     CORS(app)
     socketio.run(app)
